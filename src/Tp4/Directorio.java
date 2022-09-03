43d5ae20c262cfdfbae805ae240233a1db878492
Andres Carrera <pac2664@gmail.com>
2022-09-02 22:19:23 -0300

    Correcciones

--- src/Tp4/Directorio.java
+++ src/Tp4/Directorio.java
@@ -1,28 +1,65 @@
 package Tp4;
 
-public class Directorio {
-    public String dni;
-    public Cliente cliente;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Directorio{
+  private HashMap<Long, Cliente> mapaCliente = new HashMap<>();
 
     public Directorio() {
     }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-    
-    
-    
+  public void agregarCliente (long tel, Cliente client){
+    mapaCliente.put(tel, client);
+  }
+   public Cliente buscarCliente(long tel){
+    return mapaCliente.get(tel);
+   } 
+   public Set<Long> buscarTelefono(String Apell) {
+     Set<Long> telefonos = new HashSet <>();
+     Set<Long> claves =mapaCliente.keySet();
+     Iterator it = claves.iterator();
+     Cliente clie;
+     while (it.hasNext()){
+         long tell = (Long) it.next();
+         clie = mapaCliente.get(tell);
+         if (clie.getApellido().equals(Apell)){
+             telefonos.add(tell);
+             }
+           }
+         return telefonos;
+   }
+         
+   public ArrayList<Cliente> buscarClientes(String city){
+     ArrayList <Cliente> clientes = new ArrayList<>();
+     Set<Long> claves = mapaCliente.keySet();
+     Iterator it = claves.iterator();
+     Cliente clie;
+        while(it.hasNext()){
+            long tell = (Long)it.next();
+            clie = mapaCliente.get(tell);
+            if (clie.getCiudad().equals(city)) {
+                clientes.add(clie);
+            }
+          }
+    return clientes;    
+   }
+   public void borrarCliente(int dni){
+     Set<Long> claves = mapaCliente.keySet();
+     Iterator it = claves.iterator();
+     Cliente clie;
+     while (it.hasNext()){
+         long tell = (Long)it.next();
+         clie = mapaCliente.get(tell);
+         if (clie.getDni() == (dni)){
+             it.remove();
+         }
+     }
+   }
+   
 }
+    
+    
+    
--- src/Tp4/Tp4.java
+++ src/Tp4/Tp4.java
@@ -3,7 +3,7 @@ package Tp4;
 public class Tp4 {
 
     public static void main(String[] args) {
-       
+      
     }
     
 }
